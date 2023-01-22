f537052094f1a4df4935a211908e109b2440c387
SaemHasan <1705027@ugrad.cse.buet.ac.bd>
2023-01-22 21:06:15 +0600

    run RAxML shell added

--- run_for_different_range.sh
+++ run_for_different_range.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-DataList="1-2, 3-4, 5-6, 7-8, 9-10, 11-12, 13-14, 15-16, 17-18, 19-20, 21-22, 23-24, 25-26, 27-28, 29-30"
+# DataList="1-2, 3-4, 5-6, 7-8, 9-10, 11-12, 13-14, 15-16, 17-18, 19-20, 21-22, 23-24, 25-26, 27-28, 29-30"
 #DataList="1-2, 3-4, 5-6, 7-8, 9-10, 11-12, 13-14, 15-16, 17-18, 19-20"
+DataList="8-15, 12-14"
 Field_Separator=$IFS
 
 # set comma as internal field separator for the string list
@@ -9,7 +10,7 @@ mkdir -p terrace_output
 for val in $DataList;
 do
  x=$val
- ./run_preProcessor.sh $x > terrace_output/$x.txt
+ ./run_RAxML_NG.sh $x> terrace_output/$x.txt
 done
 
 IFS=$Field_Separator
