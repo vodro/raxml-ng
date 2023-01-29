46391730a85de16e28bf388086f1fce84063fd1f
vodro <vdrkowshic@gmail.com>
2023-01-29 22:33:02 +0600

    python3 added

--- run_RAxML_NG.sh
+++ run_RAxML_NG.sh
@@ -1,5 +1,5 @@
 cp out/incomplete/$1/astral/1_gt_species.out.tre input.nwk
 cp out/incomplete/$1/gt/1_gt.tre gt.tre
 jupyter nbconvert --to script processor.ipynb
-python processor.py
+python3 processor.py
 ./bin/raxml-ng input.nwk input.data
